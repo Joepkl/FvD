7001270180a420443d384035b001892e51986f02
Joepkl <joep.klaassen9@gmail.com>
2023-01-24 21:22:46 +0100

    .

--- scripts/index.js
+++ scripts/index.js
@@ -10,4 +10,43 @@ function openMenu(){
     navItems.classList.toggle('open2')
 }
 
+
+
+
+function animatie(){
+
+}
+
+
+
+// Scrollbased animatie INTERSECTION OBSERVER
+function createObserver() {
+    let options = {
+    //   root: null,
+    //   rootMargin: "0px",
+        threshold: 0
+    };
+
+    let observer
+    observer = new IntersectionObserver(callback, options)
+
+    const boxElement = document.querySelectorAll('main section div')
+    for (let target of boxElement){
+        observer.observe(target)
+    }
+}
+  
+createObserver()
+
+function callback(entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log('in beeld') 
+        entry.target.classList.add('test')
+      } else {
+        console.log('uit beeld')
+        entry.target.classList.remove('test')
+      }
+    });
+  }
   
